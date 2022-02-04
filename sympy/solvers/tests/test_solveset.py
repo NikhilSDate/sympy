7e688faef6c335df3f78fc23fb5b3c49c092ad29
NikhilSDate <nikhil.s.date@gmail.com>
2022-02-04 18:14:26 +0530

    fixed code quality

--- sympy/solvers/tests/test_solveset.py
+++ sympy/solvers/tests/test_solveset.py
@@ -3100,4 +3100,3 @@ def test_issue_21890():
 def test_issue_22628():
     assert nonlinsolve([h - 1, k - 1, f - 2, f - 4, -2*k], h, k, f) == S.EmptySet
     assert nonlinsolve([x**3 - 1, x + y, x**2 - 4], [x, y]) == S.EmptySet
-
