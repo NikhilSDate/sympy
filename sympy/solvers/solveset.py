5edbca23f1316ccfa50d9619c50ebc6dc463eb39
NikhilSDate <nikhil.s.date@gmail.com>
2022-03-22 16:27:04 +0530

    don't recompute basis if no solutions

--- sympy/solvers/solveset.py
+++ sympy/solvers/solveset.py
@@ -3493,7 +3493,7 @@ def _handle_poly(polys, symbols):
 
     basis = groebner(polys, symbols, polys=False, order='grevlex')
     poly_sol = [{}]
-
+    poly_eqs = list(basis)
     # Does the polynomial system have a finite number of solutions?
     if basis.is_zero_dimensional:
         # Convert Groebner basis to lex ordering
@@ -3510,8 +3510,8 @@ def _handle_poly(polys, symbols):
             pass
         else:
             return [dict(zip(symbols, res)) for res in result], []
-
-    poly_eqs = list(groebner(polys, symbols, polys=False))
+    if 1 not in poly_eqs:
+        poly_eqs = list(groebner(polys, symbols, polys=False))
     return poly_sol, poly_eqs
 
 
